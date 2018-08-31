f69ecebf56e39cd3834adb59aadf3ecca57a47e4
Dinesh Prasanth M K <dmoluguw@redhat.com>
2018-08-31 14:53:00 -0500

    Reorganizing travis scripts
    - More room to support other packages
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- buildCOPR.sh
+++ buildCOPR.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-# Build src rpm from cloned repo
-docker exec -i ${TASK} \
-    /pki/build.sh \
-    --with-timestamp \
-    --with-commit-id \
-    --work-dir=/packages srpm
+display_usage() {
+	echo -e "Usage:\n./buildCOPR SCRIPT [arguments]"
+}
 
-# Build RPMs in COPR
+if [ $# -lt 1 ]; then
+    echo "Error: Pass build script name" >> /dev/stderr
+    display_usage
+    exit 1
+fi
+
+# Build src rpm from cloned repo
+BUILD_SCRIPT=${1}
+shift
+ARGUMENTS=$@
+
+echo "Build script being executed: "${BUILD_SCRIPT}
+echo "Arguments passed to build script: "${ARGUMENTS}
 docker exec -i ${TASK} \
-    bash -c 'copr build $COPR_REPO /packages/SRPMS/*.src.rpm'
+    ${BUILD_SCRIPT} ${ARGUMENTS}
+
+# # Build RPMs in COPR
+docker exec -i ${TASK} \
+   bash -c 'copr build $COPR_REPO ${DEST_PKG_DIR}/SRPMS/*.src.rpm'
---
+++ buildInit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pull the corresponding Fedora image
+docker pull ${IMAGE}
+
+# Start the container
+docker run \
+    --detach \
+    --name=${TASK} \
+    --privileged \
+    -e BUILDUSER_UID=$(id -u) \
+    -e BUILDUSER_GID=$(id -g) \
+    -e COPR_API="${COPR_API}" \
+    -e COPR_REPO="${COPR_REPO}" \
+    -e DEST_PKG_DIR="${DEST_PKG_DIR}" \
+    -e TRAVIS=${TRAVIS} \
+    -e TRAVIS_JOB_NUMBER=${TRAVIS_JOB_NUMBER} \
+    -v $(pwd):/tmp \
+    -ti \
+    ${IMAGE}
