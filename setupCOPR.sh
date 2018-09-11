7dd6a534ba8272361e7011a240da7fb693c53360
Dinesh Prasanth M K <dmoluguw@redhat.com>
2018-09-11 16:48:59 -0400

    Adding nuxwdog and rebasing
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- setupCOPR.sh
+++ setupCOPR.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker exec -it ${TASK} bash -c 'mkdir -p ~/.config/ && echo -e ${COPR_API} > ~/.config/copr'
-docker exec -it ${TASK} git clone ${GITHUB_REPO} ${WORKING_DIR}
+docker exec -it ${TASK} git clone ${GITHUB_REPO}
