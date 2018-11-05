8b0826fe3dc1e407036590f886dcd0fdac0299ea
Endi S. Dewata <edewata@redhat.com>
2018-11-05 19:04:48 +0100

    Updated global variables
    The COPR_REPO variable has been replaced with TEST_COPR_REPO to
    prevent collision with pki's COPR_REPO variable. The PKI_VERSION
    variable has been replaced with COPR_REPO to match pki project.
    
    The setupCOPR.sh has been modified to validate that the COPR_API
    environment variable is defined.

--- buildCOPR.sh
+++ buildCOPR.sh
@@ -2,4 +2,4 @@
 
 # Build RPMs in COPR
 docker exec -i ${TASK} \
-   bash -c 'copr build $COPR_REPO ${DEST_PKG_DIR}/SRPMS/*.src.rpm'
+   bash -c 'copr build $TEST_COPR_REPO ${DEST_PKG_DIR}/SRPMS/*.src.rpm'
