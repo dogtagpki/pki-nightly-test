f12327b605ceae4a62f7353a116b9a5ec0b680e4
Dinesh Prasanth M K <dmoluguw@redhat.com>
2019-03-28 15:44:58 -0400

    Reconfigured nightly tests to run ONLY IPA tests
    Since COPR has been configured to do per-commit based
    automated builds, there is no use in having nightly builds.
    But, IPA smoke tests will need to continue running based on
    what we have in COPR.
    
    Configured this repo to run again BOTH `10.6` and `master` branches
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- buildInstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker exec -it ${TASK} dnf install -y git dnf-plugins-core rpm-build copr-cli
-docker exec -it ${TASK} dnf copr enable -y ${COPR_REPO}
-docker exec -it ${TASK} dnf -y update
